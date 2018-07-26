00c41c4af20d7af97fe31e90eb231b7d180b6481
Cory Wynn <corywynn1993@gmail.com>
2018-07-26 14:15:05 +0100

    Added base logging interface and class

---
+++ Helpers/Logging/Logger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Helpers.Logging.Interfaces;
+
+namespace Helpers.Logging
+{
+    class Logger : ILogger
+    {
+    }
+}
