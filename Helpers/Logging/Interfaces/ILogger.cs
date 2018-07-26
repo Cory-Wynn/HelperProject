c76c113c81b4b053f8b0bc6a408eaab5b42f48bb
Cory Wynn <corywynn1993@gmail.com>
2018-07-26 14:47:09 +0100

    Implemented basic txt file logger

--- Helpers/Logging/Interfaces/ILogger.cs
+++ Helpers/Logging/Interfaces/ILogger.cs
@@ -8,5 +8,10 @@ namespace Helpers.Logging.Interfaces
 {
     interface ILogger
     {
+        void Error(string text);
+
+        void Warning(string text);
+
+        void Info(string text);
     }
 }
--- Helpers/Logging/Logger.cs
+++ Helpers/Logging/Logger.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Helpers.Logging.Interfaces;
@@ -9,5 +11,110 @@ namespace Helpers.Logging
 {
     class Logger : ILogger
     {
+        private readonly string _datetimeFormat;
+        private readonly string _fileName;
+
+        /// <summary>
+        /// Initialize a new instance of SimpleLogger class.
+        /// Log file will be created automatically if not yet exists, else it can be either a fresh new file or append to the existing file.
+        /// Default is create a fresh new log file.
+        /// </summary>
+        /// <param name="append">True to append to existing log file, False to overwrite and create new log file</param>
+        public Logger(bool append = true)
+        {
+            _datetimeFormat = "yyyy-dd-MM HH:mm:ss.fff";
+            _fileName = Assembly.GetExecutingAssembly().GetName().Name + ".log";
+
+            // Log file header line
+            string logHeader = _fileName + " created.";
+            if (!File.Exists(_fileName))
+            {
+                WriteLine(DateTime.Now.ToString(_datetimeFormat) + " " + logHeader, false);
+            }
+            else
+            {
+                if (append == false)
+                    WriteLine(DateTime.Now.ToString(_datetimeFormat) + " " + logHeader, false);
+            }
+        }     
+
+        /// <summary>
+        /// Log an error message
+        /// </summary>
+        /// <param name="text">Message</param>
+        public void Error(string text)
+        {
+            WriteFormattedLog(LogLevel.ERROR, text);
+        }
+
+        /// <summary>
+        /// Log an info message
+        /// </summary>
+        /// <param name="text">Message</param>
+        public void Info(string text)
+        {
+            WriteFormattedLog(LogLevel.INFO, text);
+        }
+
+        /// <summary>
+        /// Log a waning message
+        /// </summary>
+        /// <param name="text">Message</param>
+        public void Warning(string text)
+        {
+            WriteFormattedLog(LogLevel.WARNING, text);
+        }
+
+        /// <summary>
+        /// Format a log message based on log level
+        /// </summary>
+        /// <param name="level">Log level</param>
+        /// <param name="text">Log message</param>
+        private void WriteFormattedLog(LogLevel level, string text)
+        {
+            string pretext;
+            switch (level)
+            {
+                case LogLevel.INFO: pretext = DateTime.Now.ToString(_datetimeFormat) + " [INFO]    "; break;
+                case LogLevel.WARNING: pretext = DateTime.Now.ToString(_datetimeFormat) + " [WARNING] "; break;
+                case LogLevel.ERROR: pretext = DateTime.Now.ToString(_datetimeFormat) + " [ERROR]   "; break;
+
+                default: pretext = ""; break;
+            }
+
+            WriteLine(pretext + text);
+        }
+
+        /// <summary>
+        /// Write a line of formatted log message into a log file
+        /// </summary>
+        /// <param name="text">Formatted log message</param>
+        /// <param name="append">True to append, False to overwrite the file</param>
+        /// <exception cref="System.IO.IOException"></exception>
+        private void WriteLine(string text, bool append = true)
+        {
+            try
+            {
+                using (StreamWriter Writer = new StreamWriter(_fileName, append, Encoding.UTF8))
+                {
+                    if (text != "") Writer.WriteLine(text);
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Supported log level
+        /// </summary>
+        [Flags]
+        private enum LogLevel
+        {          
+            INFO,
+            WARNING,
+            ERROR
+        }
     }
 }
