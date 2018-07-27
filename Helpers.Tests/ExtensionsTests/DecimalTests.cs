2c46a45e84710fda9812296cff164d00eb7f1dc5
Cory Wynn <corywynn1993@gmail.com>
2018-07-27 12:14:15 +0100

    Added DecimalTests

---
+++ Helpers.Tests/ExtensionsTests/DecimalTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Helpers.Extensions;
+
+namespace Helpers.Tests.ExtensionsTests
+{
+    [TestClass]
+    public class DecimalTests
+    {
+        [TestMethod]
+        public void ToNumberFormatTest()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+
+        }
+
+        [TestMethod]
+        public void ToPercentageFormat()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+
+        }
+    }
+}
