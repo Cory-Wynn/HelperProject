1a5ad855f22e946c3a6cd7270c12906801109c94
Cory Wynn <corywynn1993@gmail.com>
2018-08-06 11:05:42 +0100

    Added string extension method and test

---
+++ Helpers.Tests/ExtensionsTests/StringTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Helpers.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Helpers.Tests.ExtensionsTests
+{
+    [TestClass]
+    public class StringTests
+    {
+        [TestMethod]
+        public void RemoveWhiteSpace()
+        {
+            //Arrange
+            var s = "Hello World!";
+
+            //Act
+            var result = StringExtensions.RemoveWhiteSpace(s);
+
+            //Assert
+            Assert.AreEqual("HelloWorld!", result);
+
+        }
+    }
+}
--- Helpers/Extensions/StringExtensions.cs
+++ Helpers/Extensions/StringExtensions.cs
@@ -2,11 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Helpers.Extensions
 {
     public static class StringExtensions
     {
+        /// <summary>
+        /// Removes all white space in a string
+        /// </summary>
+        /// <param name="str">The string.</param>
+        /// <returns>The string with white space removed.</returns>
+        public static string RemoveWhiteSpace(string str)
+        {
+            return Regex.Replace(str, @"\s+", "");
+        }
     }
 }
